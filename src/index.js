8927de9e31eb6197cb55d6aba996ea4857b5a5f7
Pavel <pavel.makk.old@gmail.com>
2020-01-25 20:28:52 +0200

    Change favicon

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import Game from './components/Game/Game';
 import './index.scss';
 
-// TODO: change default logo icon
-console.log('change default logo icon')
-
 ReactDOM.render(
     <Game />, 
     document.getElementById('root')
