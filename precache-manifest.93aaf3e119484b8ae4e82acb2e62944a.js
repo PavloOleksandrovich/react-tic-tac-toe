282d3bb4a1b924d25010dbd96ed37f4fb166fd87
Pavel <pavel.makk.old@gmail.com>
2020-01-25 20:48:49 +0200

    Updates

---
+++ precache-manifest.93aaf3e119484b8ae4e82acb2e62944a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061f0552c763a0ff0b96591f2d9cea43",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a6b5a7e550a19da2aa07",
+    "url": "/react-tic-tac-toe/static/css/main.ef0db6ea.chunk.css"
+  },
+  {
+    "revision": "4c2a86b850620d6c1fed",
+    "url": "/react-tic-tac-toe/static/js/2.76a7d640.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tic-tac-toe/static/js/2.76a7d640.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6b5a7e550a19da2aa07",
+    "url": "/react-tic-tac-toe/static/js/main.ad4c89e1.chunk.js"
+  },
+  {
+    "revision": "afdfbb5364e0db92f02f",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.40ee4384.js"
+  }
+]);
